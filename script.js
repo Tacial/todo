f3da15225022ca8cdfc68704a0c4871f59917c17
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2022-11-23 20:36:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const input = document.querySelector("#text");
+const button = document.querySelector("#btn-add-todo");
+const list = document.querySelector("#todo-list");
+
+function removeItem(event) {
+    event.target.parentElement.remove();
+}
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const todo = input.value;
+
+    const item = document.createElement("li");
+    item.textContent = todo;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "x";
+    removeButton.addEventListener("click", removeItem);
+
+    item.append(removeButton);
+    list.appendChild(item)
+
+    input.value = "";
+    input.focus();
+});
